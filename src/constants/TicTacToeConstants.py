34acc8a053effaa95d57b3b4b770817e7f52abb1
CyFox66 <marler.b@northeastern.edu>
2024-06-18 12:40:47 -0400

    started tictactoe

---
+++ src/constants/TicTacToeConstants.py
@@ -0,0 +1,21 @@
+import pygame
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+
+from src.constants.GamesConstants import *
+
+pygame.init()
+
+# name of this game
+TICTACTOE_TEXT = GAMENAME_TEXT_FONT.render(TICTACTOE, True, GAMENAME_TEXT_COLOR)
+
+# score constants
+SCORE_SIZE = SCREEN_WIDTH / 30
+SCORE_FONT = pygame.font.Font(None, int(SCORE_SIZE))
+SCORE_COLOR = (255, 255, 255)
+
+SIZE_TO_SCORE = SCREEN_AREA / 250000
+
+# unique constants for this game
---
+++ src/games/TicTacToe.py
@@ -0,0 +1,94 @@
+import pygame
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+
+from src.constants.TicTacToeConstants import *
+from src.classes.GameMenu import *
+from src.classes.highscores import *
+
+pygame.init()
+
+# Game class
+class TicTacToe():
+    def __init__(self):
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.gameMenu = GameMenu(self.screen, TICTACTOE_TEXT)
+        self.highScore = load_high_score(TICTACTOE)
+        self.score = 0
+        self.gameEnd = True
+        self.pause = False
+        pygame.display.set_caption('Template Game')
+    
+    # makes scene and updates display by calling flip()
+    # Effect: draws everything onto the screen (overwriting previous items)
+    def makeScene(self):
+        self.screen.fill((255, 255, 255))
+
+        scoreText = SCORE_FONT.render('High Score: ' + str(self.highScore) + '  Score: ' + str(self.score), True, SCORE_COLOR)
+        
+        if (self.gameEnd):
+            self.gameMenu.drawPlay(scoreText)
+        elif (self.pause):
+            self.gameMenu.drawPause(scoreText)
+        else:
+            pass
+
+        pygame.display.flip()
+    
+    # this is called every frame
+    # Effect: moves the fish and updates the score
+    def onTick(self):
+        if (self.gameEnd):
+            pass
+        elif (self.pause):
+            pass
+        else:
+            pass
+        
+        if (self.highScore < self.score):
+            save_high_score(TICTACTOE, self.highScore)
+            self.highScore = self.score
+    
+    # Effect: changes our direction parameters or resets the game
+    def onKeyEvent(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                self.pause = True
+        if event.type == pygame.KEYUP:
+            pass
+
+    def onMouseEvent(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if (self.gameEnd and self.gameMenu.playButtonClicked(event.pos)):
+                self.reset()
+            if (self.pause and self.gameMenu.resumeButtonClicked(event.pos)):
+                self.pause = False
+
+    # Effect: resets the game
+    def reset(self):
+        self.score = 0
+        self.gameEnd = False
+
+    # Runs the game
+    def run(self):
+        clock = pygame.time.Clock()
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    return QUIT
+                self.onKeyEvent(event)
+                self.onMouseEvent(event)
+                if self.gameMenu.mainMenuButtonClicked(event):
+                    running = False
+                    return MAINMENU
+            
+            self.onTick()
+            self.makeScene()
+            clock.tick(TICK_SPEED)
+        
+        pygame.quit()
+        sys.exit()
